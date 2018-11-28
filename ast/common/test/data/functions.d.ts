7fa8df7225217c6685521a510bf4bac9e32718a1
Shagen Ogandzhanian <shabunc@gmail.com>
2018-11-28 15:24:11 +0100

    Add missing test resouce (functions.d.ts)

---
+++ ast/common/test/data/functions.d.ts
@@ -0,0 +1,6 @@
+declare function withoutArguments();
+// declare function withOneAny(a: any): any;
+// declare function withOneString(s: string): string;
+// declare function withOneNumber(num: number): number;
+// declare function withOneBoolean(bool: boolean): boolean;
+//declare function withManyArguments(s: string, settings: JQueryAjaxSettings): boolean;
