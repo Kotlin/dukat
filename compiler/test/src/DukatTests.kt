e097587f6e6d2bcc99b5514e913375978d775d11
Shagen Ogandzhanian <shabunc@gmail.com>
2019-01-11 16:03:59 +0100

    Introduce DukatTest as a base class for all standard Tests

---
+++ compiler/test/src/DukatTests.kt
@@ -0,0 +1,30 @@
+import org.jetbrains.dukat.compiler.Translator
+import org.jetbrains.dukat.compiler.compile
+import org.jetbrains.dukat.compiler.createV8Translator
+import java.io.File
+import kotlin.test.assertEquals
+
+open class DukatTests {
+    companion object {
+        val translator: Translator
+
+        init {
+            translator = createV8Translator()
+//            translator = createNashornTranslator()
+        }
+
+    }
+
+    protected fun assertContentEquals(name: String) {
+
+        val resourceDirectory = File("./test/data")
+        val fileNameSource = resourceDirectory.resolve("${name}.d.ts").absolutePath
+        val fileNameTarget = resourceDirectory.resolve("${name}.d.kt")
+
+        assertEquals(
+                compile(fileNameSource, translator),
+                fileNameTarget.readText().trimEnd()
+        )
+    }
+
+}
--- compiler/test/src/FunctionTests.kt
+++ compiler/test/src/FunctionTests.kt
@@ -1,36 +1,7 @@
-import org.jetbrains.dukat.compiler.Translator
-import org.jetbrains.dukat.compiler.compile
-import org.jetbrains.dukat.compiler.createV8Translator
-import java.io.File
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 
-class FunctionTests {
-
-    companion object {
-        val translator: Translator
-
-        init {
-            translator = createV8Translator()
-//            translator = createNashornTranslator()
-        }
-
-    }
-
-
-    private fun assertContentEquals(name: String) {
-
-        val resourceDirectory = File("./test/data")
-        val fileNameSource = resourceDirectory.resolve("${name}.d.ts").absolutePath
-        val fileNameTarget = resourceDirectory.resolve("${name}.d.kt")
-
-        assertEquals(
-                compile(fileNameSource, translator),
-                fileNameTarget.readText().trimEnd()
-        )
-    }
-
+class FunctionTests : DukatTests() {
     @Test
     fun testFunctions() {
         assertContentEquals("topLevelMembers/functions/functions")
@@ -48,11 +19,6 @@ class FunctionTests {
 
     @Test
     fun functionsWithOptionalParameter() {
-        assertContentEquals("topLevelMembers/functions/functionsWithOptionalParameter")
-    }
-
-    @Test
-    fun functionsWithOptionalParameter2() {
         assertContentEquals("topLevelMembers/functions/functionsWithOptionalParameter")
     }
 
