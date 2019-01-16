11596165465eb4d4f14a8cb5cf88b7ab0f5692dc
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-01-16 18:15:10 +0100

    Lower primitives in type parameters

--- compiler/src/org/jetbrains/dukat/compiler/lowerings/lowerPrimitives.kt
+++ compiler/src/org/jetbrains/dukat/compiler/lowerings/lowerPrimitives.kt
@@ -2,18 +2,30 @@ package org.jetbrains.dukat.compiler
 
 import org.jetbrains.dukat.ast.model.DocumentRoot
 import org.jetbrains.dukat.ast.model.TypeDeclaration
+import org.jetbrains.dukat.ast.model.TypeParameter
 import org.jetbrains.dukat.compiler.lowerings.ParameterValueLowering
 
 
+private fun mapPrimitiveValue(value: String): String {
+    return when (value) {
+        "any" -> "Any"
+        "boolean" -> "Boolean"
+        "string" -> "String"
+        "number" -> "Number"
+        else -> value
+    }
+}
+
 private class PrimitiveClassLowering : ParameterValueLowering() {
     override fun lowerTypeDeclaration(declaration: TypeDeclaration): TypeDeclaration {
-        return when (declaration.value) {
-            "any" -> declaration.copy(value = "Any", params = declaration.params.map { lowerParameterValue(it) })
-            "boolean" -> declaration.copy(value = "Boolean", params = declaration.params.map { lowerParameterValue(it) })
-            "string" -> declaration.copy(value = "String", params = declaration.params.map { lowerParameterValue(it) })
-            "number" -> declaration.copy(value = "Number", params = declaration.params.map { lowerParameterValue(it) })
-            else -> declaration.copy(params = declaration.params.map { lowerParameterValue(it) })
-        }
+        return  declaration.copy(
+                value =  mapPrimitiveValue(declaration.value),
+                params = declaration.params.map { lowerParameterValue(it) }
+        )
+    }
+
+    override fun lowerTypeParameter(declaration: TypeParameter): TypeParameter {
+        return declaration.copy(name = mapPrimitiveValue(declaration.name))
     }
 }
 
