3f61a7024c631b61ab708a52e06bbe7240349126
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-02-04 23:48:37 +0100

    Use better reflection API in InteropV8Class

--- j2v8/src/interop/InteropV8Class.kt
+++ j2v8/src/interop/InteropV8Class.kt
@@ -2,22 +2,19 @@ package org.jetbrains.dukat.j2v8.interop
 
 import com.eclipsesource.v8.V8Object
 import jodd.util.ClassUtil
-import kotlin.reflect.KFunction
+import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.jvm.javaType
 
 class InteropV8Class(val context: V8Object, val proxyObject: Any) {
 
     fun all() {
-        val defaultMethods = HashSet(listOf("equals", "hashCode", "toString"))
-        proxyObject::class.members
-                .filterIsInstance(KFunction::class.java)
-                .filter { !defaultMethods.contains(it.name) }.forEach { method ->
-                    val params = method.parameters.subList(1, method.parameters.size).map { param ->
-                        ClassUtil.getRawType(param.type.javaType)
-                    }
+        proxyObject::class.declaredMemberFunctions.forEach { method ->
+            val params = method.parameters.subList(1, method.parameters.size).map { param ->
+                ClassUtil.getRawType(param.type.javaType)
+            }
 
-                    context.registerJavaMethod(proxyObject, method.name, method.name, params.toTypedArray())
-                }
+            context.registerJavaMethod(proxyObject, method.name, method.name, params.toTypedArray())
+        }
     }
 
     fun method(methodName: String, vararg signatures: InteropV8Signature): InteropV8Class {
