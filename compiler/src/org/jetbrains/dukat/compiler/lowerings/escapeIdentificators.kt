0c65c3bf34e86d1ac2db3d1f15dbf6f74fca2085
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-02-05 11:06:30 +0100

    Escape param names in methods and functions

--- compiler/src/org/jetbrains/dukat/compiler/lowerings/escapeIdentificators.kt
+++ compiler/src/org/jetbrains/dukat/compiler/lowerings/escapeIdentificators.kt
@@ -3,6 +3,7 @@ package org.jetbrains.dukat.compiler.lowerings
 import org.jetbrains.dukat.ast.model.nodes.DocumentRootNode
 import org.jetbrains.dukat.ast.model.nodes.EnumNode
 import org.jetbrains.dukat.ast.model.nodes.FunctionNode
+import org.jetbrains.dukat.ast.model.nodes.MethodNode
 import org.jetbrains.dukat.ast.model.nodes.VariableNode
 import org.jetbrains.dukat.astCommon.TopLevelDeclaration
 import org.jetbrains.dukat.tsmodel.ParameterDeclaration
@@ -28,13 +29,23 @@ private class EscapeIdentificators : ParameterValueLowering {
     }
 
     override fun lowerFunctionNode(declaration: FunctionNode): FunctionNode {
-        return declaration.copy(name = escapeIdentificator(declaration.name))
+        return declaration.copy(
+            name = escapeIdentificator(declaration.name),
+            parameters = declaration.parameters.map {param -> lowerParameterDeclaration(param)}
+        )
+    }
+
+    override fun lowerMethodNode(declaration: MethodNode): MethodNode {
+        return declaration.copy(
+            name = escapeIdentificator(declaration.name),
+            parameters = declaration.parameters.map {param -> lowerParameterDeclaration(param)}
+        )
     }
 
     override fun lowerTopLevelDeclaration(declaration: TopLevelDeclaration): TopLevelDeclaration {
         return when (declaration) {
             is EnumNode -> declaration.copy(values = declaration.values.map {value -> value.copy(value = escapeIdentificator(value.value)) })
-            else -> declaration
+            else -> super.lowerTopLevelDeclaration(declaration)
         }
     }
 }
