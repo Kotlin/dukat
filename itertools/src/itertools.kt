4858e700d29c65daf725c88de2a1d1e617bd6c95
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-02-12 21:55:15 +0100

    Fix for cartesian product (which were reversing some entities)

--- itertools/src/itertools.kt
+++ itertools/src/itertools.kt
@@ -7,9 +7,9 @@ private fun <T> cartesian2(a: List<T>, b: List<T>): List<List<T>> {
 
 
 private fun <T> cartesian22(a: List<List<T>>, b: List<T>): List<List<T>> {
-    return a.map { ait -> b.map { bit ->
+    return a.map { ait -> b.flatMap { bit ->
         ait + listOf(bit)
-    } }.flatten()
+    } }
 }
 
 
@@ -22,7 +22,7 @@ fun <T> cartesian(vararg lists: List<T>): List<List<T>> {
             val mutableList = lists.toMutableList()
             val a = mutableList.removeAt(0)
             val b = mutableList.removeAt(0)
-            mutableList.foldRight(cartesian2(a, b)) { c, acc ->
+            mutableList.fold(cartesian2(a, b)) { acc, c ->
                 cartesian22(acc, c)
             }
         }
@@ -30,11 +30,5 @@ fun <T> cartesian(vararg lists: List<T>): List<List<T>> {
 }
 
 fun main() {
-    val a = cartesian2(listOf(1, 2), listOf(2, 3))
-    println(a)
-    val b = cartesian22(a, listOf(4,5))
-    println(b)
-    val c = cartesian(listOf(1, 2), listOf(2, 3), listOf(1, 5))
-    println(c)
-    println(cartesian2(listOf(1), listOf(1)))
+    println(cartesian( listOf("a"), listOf("b"), listOf("c"), listOf("q"), listOf("w")))
 }
