dfab34ceeb17b5ed4b9ed4b730f9b2b947241e20
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-03-07 00:24:17 +0300

    Prepare generated sources compilation phase

--- compiler/test/src/org/jetbrains/dukat/compiler/tests/GenerateCoreSetTests.kt
+++ compiler/test/src/org/jetbrains/dukat/compiler/tests/GenerateCoreSetTests.kt
@@ -1,10 +1,12 @@
 package org.jetbrains.dukat.compiler.tests
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import java.io.File
 
-class AllTestsCore : StandardTests() {
+class GenerateCoreSetTests : StandardTests() {
+    @DisplayName("core test set")
     @ParameterizedTest(name="{0}")
     @MethodSource("coreSet")
     fun withValueSource(name: String, tsPath: String, ktPath: String) {
@@ -13,13 +15,15 @@ class AllTestsCore : StandardTests() {
 
     companion object {
         fun fileSet(path: String): Array<Array<String>> {
+            val d_ts_postfix = ".d.ts"
+
             val rootFolder = File(path)
             return rootFolder.walk().filter { file ->
-                file.path.endsWith(".d.ts")
+                file.path.endsWith(d_ts_postfix)
             }.map { file ->
                 val tsPath = file.absolutePath
-                val ktPath = tsPath.replace(".d.ts", ".d.kt")
-                val descriptor = file.relativeTo(rootFolder).path.replace(path, "").replace(".d.ts", "")
+                val ktPath = tsPath.replace(d_ts_postfix, ".d.kt")
+                val descriptor = file.relativeTo(rootFolder).path.replace(path, "").replace(d_ts_postfix, "")
 
                 arrayOf(
                     descriptor,
--- compiler/test/src/org/jetbrains/dukat/compiler/tests/StandardTests.kt
+++ compiler/test/src/org/jetbrains/dukat/compiler/tests/StandardTests.kt
@@ -4,8 +4,36 @@ import org.jetbrains.dukat.compiler.createNashornTranslator
 import org.jetbrains.dukat.compiler.createV8Translator
 import org.jetbrains.dukat.compiler.output
 import org.jetbrains.dukat.compiler.translator.InputTranslator
+import org.jetbrains.kotlin.cli.common.ExitCode
+import org.jetbrains.kotlin.cli.common.arguments.K2JSCompilerArguments
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageLocation
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.cli.js.K2JSCompiler
+import org.jetbrains.kotlin.config.Services
 import java.io.File
 import kotlin.test.assertEquals
+
+
+private class TestCompileMessageCollector : MessageCollector {
+    private var myHasErrors: Boolean = false
+
+    override fun clear() {
+        myHasErrors = false
+    }
+
+    override fun hasErrors(): Boolean {
+        return false
+    }
+
+    override fun report(severity: CompilerMessageSeverity, message: String, location: CompilerMessageLocation?) {
+        if (severity.isError) {
+            myHasErrors = true
+
+            System.err.println("MESSAGE ${severity} ${message} ${location}")
+        }
+    }
+}
 
 open class StandardTests {
     companion object {
@@ -23,22 +51,50 @@ open class StandardTests {
 
     }
 
-    protected fun assertContentEquals(descriptor: String, tsPath: String, ktPath: String, output: (String, InputTranslator) -> String? = ::output) {
+    private fun compile(sourcePath: String, targetPath: String): ExitCode {
+        println("TARGET ${targetPath}")
+
+        val options =
+                K2JSCompilerArguments().apply {
+                    outputFile = targetPath
+                    metaInfo = false
+                    sourceMap = false
+                    kotlinHome = "./build"
+                }
+
+        options.freeArgs = listOf(sourcePath)
+        return K2JSCompiler().exec(
+                TestCompileMessageCollector(),
+                Services.EMPTY,
+                options
+        )
+    }
+
+    protected fun assertContentEquals(
+            descriptor: String,
+            tsPath: String,
+            ktPath: String,
+            output: (String, InputTranslator) -> String? = ::output
+    ) {
 
         val targetShortName = "${descriptor}.d.kt"
 
         val translated = output(tsPath, translator)
-        val outputDirectory = File("./build/tests/out")
-        translated?.let {
-            val outputFile = outputDirectory.resolve(targetShortName)
-            outputFile.parentFile.mkdirs()
-            outputFile.writeText(translated)
-        }
 
         assertEquals(
                 translated,
                 File(ktPath).readText().trimEnd()
         )
+
+        val outputDirectory = File("./build/tests/out")
+        translated?.let {
+            val outputFile = outputDirectory.resolve(targetShortName)
+            outputFile.parentFile.mkdirs()
+            outputFile.writeText(translated)
+
+            val sourcePath = outputFile.absolutePath
+            //assertEquals(ExitCode.OK, compile(sourcePath, sourcePath.replace(".kt", ".js")), translated)
+        }
     }
 
 }
