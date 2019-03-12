f311d53ecee133537b8a99b390d50a0dcff64393
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-03-12 17:46:48 +0100

    Removed fileContent
    it's a remnant of mpp-era

--- compiler/src/org/jetbrains/dukat/compiler/FileResolver.kt
+++ compiler/src/org/jetbrains/dukat/compiler/FileResolver.kt
@@ -1,8 +1,14 @@
 package org.jetbrains.dukat.compiler
 
+import java.io.File
+
 typealias ContentResolver = (fileName: String) -> String
 
 class FileResolver {
+
+    fun fileContent(path: String): String {
+        return File(path).bufferedReader().use { it.readText() }
+    }
 
     fun resolve(fileName: String): String {
         return fileContent(fileName)
--- compiler/src/org/jetbrains/dukat/compiler/fileContent.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.dukat.compiler
-
-import java.io.File
-
-fun fileContent(path: String): String {
-    return File(path).bufferedReader().use { it.readText() }
-}
