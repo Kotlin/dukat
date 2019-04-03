0523083303d80c4d008b73b5ecaae119625f1dd1
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-04-03 13:15:04 +0300

    Update "overrides.d.ts" test case with generalized method

--- compiler/test/data/class/inheritance/overrides.d.kt
+++ compiler/test/data/class/inheritance/overrides.d.kt
@@ -6,11 +6,13 @@ external interface BaseEvent {
     var data: dynamic /* String | Number */
     fun getDelegateTarget(): Shape
     fun getElement(): Element
+    fun <T : Shape> transform(shape: T? = definedExternally /* null */): T
 }
 external open class BoxStringEvent : BaseEvent {
     override var data: String = definedExternally
     override fun getDelegateTarget(): Box = definedExternally
     override fun getElement(): HTMLElement = definedExternally
+    override fun <T : Shape> transform(shape: T?): T = definedExternally
 }
 external interface NumberEvent : BaseEvent {
     override var data: Number
--- compiler/test/data/class/inheritance/overrides.d.ts
+++ compiler/test/data/class/inheritance/overrides.d.ts
@@ -6,11 +6,13 @@ declare interface BaseEvent {
     data: string | number;
     getDelegateTarget(): Shape;
     getElement(): Element;
+    transform<T extends Shape>(shape?: T): T;
 }
 declare class BoxStringEvent implements BaseEvent {
     data: string;
     getDelegateTarget(): Box;
     getElement(): HTMLElement;
+    transform<T extends Shape>(shape?: T): T;
 }
 declare interface NumberEvent extends BaseEvent {
     data: number;
