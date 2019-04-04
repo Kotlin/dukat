d63004df6ec76e5acccaa3efeb9efe25df741dc2
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-04-04 13:12:23 +0300

    Support running tests from gradle

--- compiler/test/src/org/jetbrains/dukat/compiler/tests/GenerateCoreSetTests.kt
+++ compiler/test/src/org/jetbrains/dukat/compiler/tests/GenerateCoreSetTests.kt
@@ -6,19 +6,20 @@ import org.junit.jupiter.params.provider.MethodSource
 import java.io.File
 
 class GenerateCoreSetTests : StandardTests() {
+
     @DisplayName("core test set")
-    @ParameterizedTest(name="{0}")
+    @ParameterizedTest(name = "{0}")
     @MethodSource("coreSet")
     fun withValueSource(name: String, tsPath: String, ktPath: String) {
         assertContentEquals(name, tsPath, ktPath)
     }
 
-    @DisplayName("core test set compile")
-    @ParameterizedTest(name="{0}")
-    @MethodSource("coreSet")
-    fun withValueSourceCompiled(name: String, tsPath: String, ktPath: String) {
-        assertContentCompiles(name, tsPath)
-    }
+//    @DisplayName("core test set compile")
+//    @ParameterizedTest(name = "{0}")
+//    @MethodSource("coreSet")
+//    fun withValueSourceCompiled(name: String, tsPath: String, ktPath: String) {
+//        assertContentCompiles(name, tsPath)
+//    }
 
     companion object {
         fun fileSet(path: String): Array<Array<String>> {
@@ -33,9 +34,9 @@ class GenerateCoreSetTests : StandardTests() {
                 val descriptor = file.relativeTo(rootFolder).path.replace(path, "").replace(d_ts_postfix, "")
 
                 arrayOf(
-                    descriptor,
-                    tsPath,
-                    ktPath
+                        descriptor,
+                        tsPath,
+                        ktPath
                 )
             }.toList().toTypedArray()
         }
