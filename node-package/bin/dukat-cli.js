d8ebabf45934709f12a61467ad32c9fe4097965f
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-05-17 13:02:57 +0300

    [cli] Check whether java is available in a separate step

--- node-package/bin/dukat-cli.js
+++ node-package/bin/dukat-cli.js
@@ -3,15 +3,24 @@
 var exec = require('child_process').exec;
 var path = require('path');
 
+function isWin() {
+    return process.platform == "win32";
+}
+
 var printError = function(errorMessage) {
     console.error("ERROR: " + errorMessage);
 };
 
 var processError = function(error) {
+    printError("code: " + error.code);
     printError(error.message);
-    if ((error.code == 127) || (error.code == 1)) {
-        printError("It looks like you don't have java installed or it's just not reachable from command-line");
-        printError("As of now \"dukat\" requires Java Runtime Environment to be installed.");
+    if (!isWin()) {
+        if (error.code == 1) {
+            printError("File not found");
+        } else if (error.code == 127) {
+            // actually never supposed to be here because we have guardJavaExists()
+            printNoJava()
+        }
     }
 };
 
@@ -28,6 +37,20 @@ var run = function(command) {
     exec(command, execHandler);
 };
 
+function printNoJava() {
+    printError("It looks like you don't have java installed or it's just not reachable from command-line");
+    printError("As of now \"dukat\" requires Java Runtime Environment to be installed.");
+}
+
+function guardJavaExists() {
+    exec("java -version", function(error, stdout, stderr) {
+        if (error) {
+            printNoJava();
+            process.exit(error.code);
+        }
+    });
+}
+
 var main = function() {
     var args = process.argv.slice(2);
 
@@ -36,6 +59,7 @@ var main = function() {
     var cliPath = path.resolve(runtimePath, "dukat-cli.jar");
     var classPath = [jsPath, cliPath].join(path.delimiter);
 
+    guardJavaExists();
     run("java -cp " + classPath + " org.jetbrains.dukat.cli.CliKt " + args.join(" "))
 };
 
