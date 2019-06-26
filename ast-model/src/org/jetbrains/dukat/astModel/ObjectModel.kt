5e9feeb1ac24eeaf281c1dd687dbc80dc9eec2dc
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-06-26 17:30:53 +0200

    ObjectModel is not extending ClassLikeNode

--- ast-model/src/org/jetbrains/dukat/astModel/ObjectModel.kt
+++ ast-model/src/org/jetbrains/dukat/astModel/ObjectModel.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.dukat.astModel
 
-import org.jetbrains.dukat.ast.model.nodes.ClassLikeNode
 import org.jetbrains.dukat.ast.model.nodes.TopLevelNode
 import org.jetbrains.dukat.astCommon.NameEntity
 
@@ -9,4 +8,4 @@ data class ObjectModel(
         val members: List<MemberModel>,
 
         val parentEntities: List<HeritageModel>
-) : ClassLikeNode, TopLevelNode
+) : TopLevelNode
