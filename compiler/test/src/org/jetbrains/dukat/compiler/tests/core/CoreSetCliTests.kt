8635a3d7e34ede715d870184f62bfcbffb831f7e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-10-25 11:52:51 +0200

    Add CoreSetCliTests to default tests
    But introduce coreSets property for those who don't want to run longer set of tests

--- compiler/test/src/org/jetbrains/dukat/compiler/tests/core/CoreSetCliTests.kt
+++ compiler/test/src/org/jetbrains/dukat/compiler/tests/core/CoreSetCliTests.kt
@@ -27,7 +27,6 @@ class CoreSetCliTests {
     @DisplayName("core test set [cli run]")
     @ParameterizedTest(name = "{0}")
     @MethodSource("coreSet")
-    @EnabledIfSystemProperty(named = "dukat.test.cli", matches = "true")
     fun withValueSource(name: String, tsPath: String, ktPath: String) {
         assertContentEqualsBinary(name, tsPath, ktPath)
     }
@@ -40,7 +39,7 @@ class CoreSetCliTests {
 
         @JvmStatic
         fun coreSet(): Array<Array<String>> {
-            return fileSetWithDescriptors("./test/data/typescript/aaa")
+            return fileSetWithDescriptors("./test/data/typescript")
         }
     }
 
