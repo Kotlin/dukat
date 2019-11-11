439abd60b7afa3d3055cbeeea98823e15108f92f
unknown <seitz.m95@gmail.com>
2019-11-11 10:27:02 +0100

    Add TODO for CallResultConstraint, to make it mutable

--- javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/constraint/immutable/call/CallResultConstraint.kt
+++ javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/constraint/immutable/call/CallResultConstraint.kt
@@ -1,7 +1,6 @@
 package org.jetbrains.dukat.js.type.constraint.immutable.call
 
 import org.jetbrains.dukat.js.type.constraint.Constraint
-import org.jetbrains.dukat.js.type.constraint.composite.CompositeConstraint
 import org.jetbrains.dukat.js.type.constraint.immutable.ImmutableConstraint
 import org.jetbrains.dukat.js.type.constraint.immutable.resolved.VoidTypeConstraint
 import org.jetbrains.dukat.js.type.property_owner.PropertyOwner
@@ -9,7 +8,7 @@ import org.jetbrains.dukat.js.type.property_owner.PropertyOwner
 data class CallResultConstraint(
         val callTarget: Constraint,
         val arguments: List<Constraint>
-) : ImmutableConstraint {
+) : ImmutableConstraint { //TODO treat this similarly to a reference constraint
     override fun resolve(owner: PropertyOwner): Constraint {
         val functionConstraint = callTarget.getResolvedFunctionConstraint(owner)
 
