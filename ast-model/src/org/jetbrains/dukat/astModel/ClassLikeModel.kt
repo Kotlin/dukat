01fa8cddc843efa1db28ec660836b4f48b35768e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2019-11-28 11:40:41 +0100

    Remove MergeableModel - it was an overkill not to say an abuse of type system

--- ast-model/src/org/jetbrains/dukat/astModel/ClassLikeModel.kt
+++ ast-model/src/org/jetbrains/dukat/astModel/ClassLikeModel.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.dukat.astModel
 
-interface ClassLikeModel : TopLevelModel, MemberModel, MergeableModel {
+interface ClassLikeModel : TopLevelModel, MemberModel {
     val typeParameters: List<TypeParameterModel>
     val members: List<MemberModel>
     val companionObject: ObjectModel?
