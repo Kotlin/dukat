10810d6cd3fc07300ca61af2b98d5e28ff28c94f
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-01-30 22:15:12 +0100

    addPackageName printing packageName )))

--- typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/addPackageName.kt
+++ typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/addPackageName.kt
@@ -4,5 +4,6 @@ import org.jetbrains.dukat.astCommon.NameEntity
 import org.jetbrains.dukat.tsmodel.SourceSetDeclaration
 
 fun SourceSetDeclaration.addPackageName(packageName: NameEntity?): SourceSetDeclaration {
+    println(packageName)
     return this
 }
