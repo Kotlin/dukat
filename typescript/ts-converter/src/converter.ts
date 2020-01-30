435425748a812f79242e896b0e4e94a1cd9d1bbb
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-01-30 17:38:28 +0100

    Resolve lib source files without calling program.isSourceFileDefaultLibrary

--- typescript/ts-converter/src/converter.ts
+++ typescript/ts-converter/src/converter.ts
@@ -38,11 +38,11 @@ class SourceBundleBuilder {
   ) {
   }
 
-  private createSourceSet(program: ts.Program, fileName: string, packageNameString: string): Array<SourceFileDeclaration> {
+  private createSourceSet(program: ts.Program, libs: Set<string>, fileName: string, packageNameString: string): Array<SourceFileDeclaration> {
     let logger = createLogger("converter");
 
     let packageName = this.astFactory.createIdentifierDeclarationAsNameEntity(packageNameString);
-    let libChecker = (node: ts.Node) => program.isSourceFileDefaultLibrary(node.getSourceFile());
+    let libChecker = (node: ts.Node) => libs.has(node.getSourceFile().fileName);
     let libVisitor = new LibraryDeclarationsVisitor(
       this.libDeclarations,
       program.getTypeChecker(),
@@ -91,8 +91,9 @@ class SourceBundleBuilder {
       throw new Error("failed to create languageService");
     }
 
+    const libs = getLibPaths(program, program.getSourceFile(this.stdLib), ts.getDirectoryPath(this.stdLib));
     let sourceSets = files.map(fileName => {
-      return this.astFactory.createSourceSet(fileName, this.createSourceSet(program, fileName, packageName));
+      return this.astFactory.createSourceSet(fileName, this.createSourceSet(program, libs, fileName, packageName));
     });
 
     let sourceSetBundle = new declarations.SourceBundleDeclarationProto();
@@ -120,6 +121,23 @@ class SourceBundleBuilder {
     sourceSetBundle.setSourcesList(sourceSets);
     return sourceSetBundle;
   }
+}
+
+function getLibPaths(program: ts.Program, libPath: ts.SourceFile | undefined, defaultLibraryPath: string, libs: Set<string> = new Set()) {
+  if (libPath === undefined) {
+    return libs;
+  }
+
+  if (libs.has(libPath.fileName)) {
+    return libs;
+  }
+
+  libs.add(libPath.fileName);
+  libPath.libReferenceDirectives.forEach(libReference => {
+    getLibPaths(program, program.getLibFileFromReference(libReference), defaultLibraryPath, libs);
+  });
+
+  return libs;
 }
 
 export function createBundle(stdlib: string, packageName: string, files: Array<string>) {
