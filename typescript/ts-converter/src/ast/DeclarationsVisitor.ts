d55b21ed84c450774b905814b93b600df47440e9
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-02-04 14:41:25 +0100

    isTransparentDependency(node: string | ts.Node): boolean {

--- typescript/ts-converter/src/ast/DeclarationsVisitor.ts
+++ typescript/ts-converter/src/ast/DeclarationsVisitor.ts
@@ -3,75 +3,75 @@ import {Declaration} from "./ast";
 
 export class DeclarationsVisitor {
 
-  private processed = new Set<ts.Node>();
-  private declarations = new Map<string, Array<Declaration>>();
+    private processed = new Set<ts.Node>();
+    private declarations = new Map<string, Array<Declaration>>();
 
-  private skipTypes = new Set([
-    "Array",
-    "Boolean",
-    "Error",
-    "Function",
-    "Number",
-    "RegExp",
-    "ReadonlyArray",
-    "String"
-  ]);
+    private skipTypes = new Set([
+        "Array",
+        "Boolean",
+        "Error",
+        "Function",
+        "Number",
+        "RegExp",
+        "ReadonlyArray",
+        "String"
+    ]);
 
-  constructor(
-      private typeChecker: ts.TypeChecker,
-      private libsSet: Set<string>,
-      private files: Array<string>,
-      public createDeclarations?: (node: ts.Node) => Array<Declaration>
-  ) {
-  }
-
-  private registerDeclaration(declaration) {
-    if (this.processed.has(declaration)) {
-      return;
+    constructor(
+        private typeChecker: ts.TypeChecker,
+        private libsSet: Set<string>,
+        private files: Array<string>,
+        public createDeclarations?: (node: ts.Node) => Array<Declaration>
+    ) {
     }
 
-    this.processed.add(declaration);
-
-    const sourceName = declaration.getSourceFile().fileName;
-
-    if (!Array.isArray(this.declarations.get(sourceName))) {
-      this.declarations.set(sourceName, []);
-    }
-
-    if (this.createDeclarations) {
-      this.declarations.get(sourceName)!.push(...this.createDeclarations(declaration));
-    }
-  }
-
-  private checkReferences(node: ts.Node) {
-    const symbol = this.typeChecker.getTypeAtLocation(node).symbol;
-    if (symbol && Array.isArray(symbol.declarations)) {
-      for (let declaration of symbol.declarations) {
-        if (this.isTransparentDependency(declaration)) {
-          this.registerDeclaration(declaration);
+    private registerDeclaration(declaration) {
+        if (this.processed.has(declaration)) {
+            return;
         }
-      }
+
+        this.processed.add(declaration);
+
+        const sourceName = declaration.getSourceFile().fileName;
+
+        if (!Array.isArray(this.declarations.get(sourceName))) {
+            this.declarations.set(sourceName, []);
+        }
+
+        if (this.createDeclarations) {
+            this.declarations.get(sourceName)!.push(...this.createDeclarations(declaration));
+        }
     }
-  }
 
-  process(node: ts.TypeNode) {
-    const shouldNotBeProcessed = ts.isTypeReferenceNode(node) && this.skipTypes.has(node.typeName.getText());
-    if (!shouldNotBeProcessed) {
-      this.checkReferences(node)
+    private checkReferences(node: ts.Node) {
+        const symbol = this.typeChecker.getTypeAtLocation(node).symbol;
+        if (symbol && Array.isArray(symbol.declarations)) {
+            for (let declaration of symbol.declarations) {
+                if (this.isTransparentDependency(declaration)) {
+                    this.registerDeclaration(declaration);
+                }
+            }
+        }
     }
-  }
 
-  public forEachDeclaration(callback: (value: Array<ts.Node>, key: string) => void) {
-    this.declarations.forEach(callback);
-  }
+    process(node: ts.TypeNode) {
+        const shouldNotBeProcessed = ts.isTypeReferenceNode(node) && this.skipTypes.has(node.typeName.getText());
+        if (!shouldNotBeProcessed) {
+            this.checkReferences(node)
+        }
+    }
 
-  isTransparentDependency(node: ts.Node): boolean {
-    const fileName = node.getSourceFile().fileName;
-    return this.files.some(directDependencyName => fileName);
-  }
+    public forEachDeclaration(callback: (value: Array<ts.Node>, key: string) => void) {
+        this.declarations.forEach(callback);
+    }
 
-  isLibDeclaration(source: ts.Node): boolean {
-    return this.libsSet.has(source.getSourceFile().fileName);
-  }
+    isTransparentDependency(node: string | ts.Node): boolean {
+        const fileName = (typeof node == "string") ? node : node.getSourceFile().fileName;
+        return this.files.some(directDependencyName => fileName);
+    }
+
+    isLibDeclaration(source: ts.Node): boolean {
+        return this.libsSet.has(source.getSourceFile().fileName);
+    }
 
 }
