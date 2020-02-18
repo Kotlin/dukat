18b8cc8ae357e816a3bdc14d95ea93a915a85644
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-02-18 22:52:47 +0100

    Test case updated (only the order of translated entities)

--- compiler/test/data/typescript/importAs/importAs.d.kt
+++ compiler/test/data/typescript/importAs/importAs.d.kt
@@ -29,29 +29,6 @@ external fun createComputable(): Computable
 external fun createPipable(): Pipable
 
 // ------------------------------------------------------------------------------------------
-@file:JsModule("_transformable")
-@file:JsNonModule
-@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
-package _transformable.internalApi
-
-import kotlin.js.*
-import kotlin.js.Json
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
-
-external interface TransformOptions
-
-// ------------------------------------------------------------------------------------------
 @file:JsModule("_computable")
 @file:JsNonModule
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
@@ -77,3 +54,27 @@ external open class Computable {
 }
 
 external interface Pipable
+
+// ------------------------------------------------------------------------------------------
+@file:JsModule("_transformable")
+@file:JsNonModule
+@file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS", "EXTERNAL_DELEGATION")
+package _transformable.internalApi
+
+import kotlin.js.*
+import kotlin.js.Json
+import org.khronos.webgl.*
+import org.w3c.dom.*
+import org.w3c.dom.events.*
+import org.w3c.dom.parsing.*
+import org.w3c.dom.svg.*
+import org.w3c.dom.url.*
+import org.w3c.fetch.*
+import org.w3c.files.*
+import org.w3c.notifications.*
+import org.w3c.performance.*
+import org.w3c.workers.*
+import org.w3c.xhr.*
+
+external interface TransformOptions
+
