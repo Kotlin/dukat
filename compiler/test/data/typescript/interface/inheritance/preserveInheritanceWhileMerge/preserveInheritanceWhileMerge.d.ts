5122b4586b45c31e0cf767aabd16239740eff4ad
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-02-20 18:22:45 +0100

    Remove withGenericParams test case since it completely covered in preserveInheritanceWhileMerge

--- compiler/test/data/typescript/interface/inheritance/preserveInheritanceWhileMerge/preserveInheritanceWhileMerge.d.ts
+++ compiler/test/data/typescript/interface/inheritance/preserveInheritanceWhileMerge/preserveInheritanceWhileMerge.d.ts
@@ -6,4 +6,9 @@ declare module "api" {
   export interface Platform<T> extends commonapi.GeneralPlatform {
     ping(): T;
   }
+
+  var Platform: {
+    new <T>(type: string, length?: number): Platform<T>;
+    <T>(type: string, length?: number): Platform<T>;
+  };
 }
