1f5e393b6560f4256d52507c6a6f05e8783e5cbe
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-03-03 14:19:32 +0100

    ExportContext incorporate actual name and kind of the node to UID
    For debug purposes only - one shall never rely on this information. Never.

--- typescript/ts-converter/src/ExportContext.ts
+++ typescript/ts-converter/src/ExportContext.ts
@@ -2,6 +2,19 @@ import {uid} from "./uid";
 import * as ts from "typescript";
 import {createLogger} from "./Logger";
 
+
+function resolveName(node: ts.Node): string | null {
+  if (ts.isIdentifier(node)) {
+    return node.text;
+  }
+
+  if (node.name && ts.isIdentifier(node.name)) {
+    return node.name.text;
+  }
+
+  return null;
+}
+
 export class ExportContext {
   private exportTable: Map<ts.Node, string> = new Map();
   private log = createLogger("ExportContext");
@@ -14,6 +27,11 @@ export class ExportContext {
       let nodeUid = uid();
       if (this.isLibNode(node)) {
         nodeUid = "lib-" + nodeUid;
+      }
+
+      let name = resolveName(node);
+      if (name) {
+        nodeUid = nodeUid + `${ts.SyntaxKind[node.kind]}-${name}`;
       }
 
       this.exportTable.set(node, nodeUid);
