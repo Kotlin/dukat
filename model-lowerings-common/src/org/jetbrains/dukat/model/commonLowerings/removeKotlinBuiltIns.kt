5e179b193f9d42ac892ec495d26766bfb5a06d9e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-03-25 00:27:39 +0100

    Filter out any extensions on entities from kotlin stdlib declared in ts stdlib

--- model-lowerings-common/src/org/jetbrains/dukat/model/commonLowerings/removeKotlinBuiltIns.kt
+++ model-lowerings-common/src/org/jetbrains/dukat/model/commonLowerings/removeKotlinBuiltIns.kt
@@ -1,8 +1,11 @@
 package org.jetbrains.dukat.model.commonLowerings
 
+import org.jetbrains.dukat.astCommon.IdentifierEntity
 import org.jetbrains.dukat.astModel.ClassLikeModel
+import org.jetbrains.dukat.astModel.FunctionModel
 import org.jetbrains.dukat.astModel.ModuleModel
 import org.jetbrains.dukat.astModel.SourceFileModel
+import org.jetbrains.dukat.astModel.VariableModel
 import org.jetbrains.dukat.stdlib.KotlinBuiltInEntities
 import org.jetbrains.dukat.stdlib.KotlinStdlibEntities
 
@@ -10,6 +13,14 @@ private fun ModuleModel.removeKotlinBuiltIns(): ModuleModel {
     return copy(declarations = declarations.filter {
         if (it is ClassLikeModel) {
             !KotlinBuiltInEntities.contains(it.name) && !KotlinStdlibEntities.contains(it.name)
+        } else if (it is FunctionModel) {
+            it.extend?.name?.let { name ->
+                !KotlinStdlibEntities.contains(name)
+            } ?: true
+        } else if (it is VariableModel) {
+            it.extend?.name?.let { name ->
+                !KotlinStdlibEntities.contains(name)
+            } ?: true
         } else {
             true
         }
