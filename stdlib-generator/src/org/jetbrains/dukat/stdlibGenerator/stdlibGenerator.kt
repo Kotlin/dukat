bdfd466d8e95a133a1a11d0320f8ca71e4a4d328
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-04-02 14:33:05 +0200

    Production-ready descrtipor -> model generation

--- stdlib-generator/src/org/jetbrains/dukat/stdlibGenerator/stdlibGenerator.kt
+++ stdlib-generator/src/org/jetbrains/dukat/stdlibGenerator/stdlibGenerator.kt
@@ -6,15 +6,22 @@ import org.jetbrains.dukat.astCommon.toNameEntity
 import org.jetbrains.dukat.astModel.ClassModel
 import org.jetbrains.dukat.astModel.FunctionTypeModel
 import org.jetbrains.dukat.astModel.InterfaceModel
+import org.jetbrains.dukat.astModel.LambdaParameterModel
 import org.jetbrains.dukat.astModel.MemberModel
+import org.jetbrains.dukat.astModel.MethodModel
 import org.jetbrains.dukat.astModel.ModuleModel
+import org.jetbrains.dukat.astModel.ParameterModel
 import org.jetbrains.dukat.astModel.PropertyModel
 import org.jetbrains.dukat.astModel.SourceFileModel
 import org.jetbrains.dukat.astModel.TopLevelModel
 import org.jetbrains.dukat.astModel.TypeModel
+import org.jetbrains.dukat.astModel.TypeParameterModel
+import org.jetbrains.dukat.astModel.TypeParameterReferenceModel
 import org.jetbrains.dukat.astModel.TypeValueModel
+import org.jetbrains.dukat.astModel.Variance
+import org.jetbrains.dukat.astModel.expressions.IdentifierExpressionModel
 import org.jetbrains.dukat.astModel.modifiers.VisibilityModifierModel
-import org.jetbrains.dukat.astModel.LambdaParameterModel
+import org.jetbrains.dukat.astModel.statements.ExpressionStatementModel
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.config.addKotlinSourceRoots
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
@@ -22,14 +29,18 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.descriptors.ClassDescriptor
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.DeclarationDescriptor
+import org.jetbrains.kotlin.descriptors.FunctionDescriptor
 import org.jetbrains.kotlin.descriptors.PackageFragmentDescriptor
 import org.jetbrains.kotlin.descriptors.PackageFragmentProvider
 import org.jetbrains.kotlin.descriptors.PropertyDescriptor
+import org.jetbrains.kotlin.descriptors.TypeParameterDescriptor
+import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
 import org.jetbrains.kotlin.descriptors.impl.ModuleDescriptorImpl
 import org.jetbrains.kotlin.incremental.components.LookupTracker
 import org.jetbrains.kotlin.js.resolve.JsPlatformAnalyzerServices
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.resolve.calls.components.isVararg
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.kotlin.resolve.descriptorUtil.parents
 import org.jetbrains.kotlin.resolve.scopes.getDescriptorsFiltered
@@ -40,6 +51,8 @@ import org.jetbrains.kotlin.storage.LockBasedStorageManager
 import org.jetbrains.kotlin.types.DynamicType
 import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.kotlin.types.SimpleType
+import org.jetbrains.kotlin.types.TypeProjection
+import org.jetbrains.kotlin.types.typeUtil.isTypeParameter
 import org.jetbrains.kotlin.utils.KotlinJavascriptMetadata
 import org.jetbrains.kotlin.utils.KotlinJavascriptMetadataUtils
 import java.io.File
@@ -93,6 +106,10 @@ private fun SimpleType.convertToFunctionTypeModel(): FunctionTypeModel {
     )
 }
 
+private fun TypeProjection.convertToTypeParameterModel(): TypeParameterModel {
+    return TypeParameterModel(type = type.convertToTypeModel(), constraints = emptyList(), variance = Variance.INVARIANT, nullable = false)
+}
+
 private fun KotlinType.convertToTypeModel(): TypeModel {
     return when (this) {
         is SimpleType -> {
@@ -101,12 +118,48 @@ private fun KotlinType.convertToTypeModel(): TypeModel {
             if (name == "Function1") {
                 convertToFunctionTypeModel()
             } else {
-                TypeValueModel(IdentifierEntity(name), emptyList(), null, null, isMarkedNullable)
+                if (isTypeParameter()) {
+                    TypeParameterReferenceModel(IdentifierEntity(name), null)
+                } else {
+                    TypeValueModel(IdentifierEntity(name), arguments.map { it.convertToTypeParameterModel() }, null, null, isMarkedNullable)
+                }
             }
         }
         is DynamicType -> TypeValueModel(IdentifierEntity("dynamic"), emptyList(), null, null, false)
         else -> throw Exception("unkown kotlin type: ${this.javaClass.simpleName}")
     }
+}
+
+private fun ValueParameterDescriptor.convertToParameterModel(): ParameterModel {
+    return ParameterModel(
+        name = name.toString(),
+        type = type.convertToTypeModel(),
+        initializer = if (this.declaresDefaultValue()) {
+            ExpressionStatementModel(
+                    IdentifierExpressionModel(
+                            IdentifierEntity("definedExternally")
+                    )
+            )
+        } else null ,
+        vararg = isVararg,
+        modifier = null
+    )
+}
+
+private fun FunctionDescriptor.convertToMethodModel(): MethodModel {
+    val override = (overriddenDescriptors.firstOrNull()?.parents?.firstOrNull() as? ClassDescriptor)?.fqNameSafe?.toString()?.toNameEntity()
+    return MethodModel(
+        name = name.toString().toNameEntity(),
+        parameters = valueParameters.map { it.convertToParameterModel() },
+        typeParameters = typeParameters.map { it.convertToTypeParameterModel() },
+        type = returnType?.convertToTypeModel() ?: TypeValueModel(IdentifierEntity("Unit"), emptyList(), null, null),
+        static = false,
+        override = override,
+        open = true,
+        body = null,
+        operator = isOperator,
+        annotations = mutableListOf()
+    )
 }
 
 private fun DeclarationDescriptor.convertToMemberModel(): MemberModel? {
@@ -115,7 +168,7 @@ private fun DeclarationDescriptor.convertToMemberModel(): MemberModel? {
             val override = (overriddenDescriptors.firstOrNull()?.parents?.firstOrNull() as? ClassDescriptor)?.fqNameSafe?.toString()?.toNameEntity()
             PropertyModel(
                     name = IdentifierEntity(name.toString()),
-                    typeParameters = emptyList(),
+                    typeParameters = typeParameters.map { it.convertToTypeParameterModel() },
                     static = false,
                     override = override,
                     immutable = !isVar,
@@ -125,6 +178,7 @@ private fun DeclarationDescriptor.convertToMemberModel(): MemberModel? {
                     open = true
             )
         }
+        is FunctionDescriptor -> convertToMethodModel()
         else -> {
             //println("UNKOWNN ${this.javaClass.simpleName}")
             null
@@ -132,12 +186,33 @@ private fun DeclarationDescriptor.convertToMemberModel(): MemberModel? {
     }
 }
 
+private fun org.jetbrains.kotlin.types.Variance.convertToVarianceModel(): Variance {
+    return when {
+        allowsInPosition && allowsInPosition -> Variance.INVARIANT
+        allowsInPosition && !allowsInPosition -> Variance.CONTRAVARIANT
+        !allowsInPosition && allowsInPosition -> Variance.COVARIANT
+        else -> Variance.INVARIANT
+    }
+}
+
+private fun TypeParameterDescriptor.convertToTypeParameterModel(): TypeParameterModel {
+    return TypeParameterModel(
+        type = TypeParameterReferenceModel(name = name.toString().toNameEntity(), metaDescription = null),
+        constraints = emptyList(),
+        variance = variance.convertToVarianceModel(),
+        nullable = false
+    )
+}
+
 private fun ClassDescriptor.convertToClassModel(): ClassModel {
+    if (declaredTypeParameters.isNotEmpty()) {
+        println("DECL Typ ${name.toString()} PARAMETERS ${declaredTypeParameters}")
+    }
     return ClassModel(
             name = IdentifierEntity(this.name.toString()),
             members = unsubstitutedMemberScope.getDescriptorsFiltered().mapNotNull { it.convertToMemberModel() },
             companionObject = null,
-            typeParameters = emptyList(),
+            typeParameters = declaredTypeParameters.map { it.convertToTypeParameterModel() },
             parentEntities = emptyList(),
             primaryConstructor = null,
             annotations = mutableListOf(),
@@ -153,7 +228,7 @@ private fun ClassDescriptor.convertToInterfaceModel(): InterfaceModel {
             name = IdentifierEntity(this.name.toString()),
             members = unsubstitutedMemberScope.getDescriptorsFiltered().mapNotNull { it.convertToMemberModel() },
             companionObject = null,
-            typeParameters = emptyList(),
+            typeParameters = declaredTypeParameters.map { it.convertToTypeParameterModel() },
             parentEntities = emptyList(),
             annotations = mutableListOf(),
             comment = null,
