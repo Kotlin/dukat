8e51ae454980b854d8fa6b43d7fb0473f5715003
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-04-08 22:31:00 +0200

    RenameImpossibleDeclarationsTypeLowering implements TopLevelDeclarationLowering

--- typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/renameImpossibleRedeclarations.kt
+++ typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/renameImpossibleRedeclarations.kt
@@ -1,16 +1,15 @@
 package org.jetbrains.dukat.tsLowerings
 
+import TopLevelDeclarationLowering
 import org.jetbrains.dukat.astCommon.IdentifierEntity
 import org.jetbrains.dukat.astCommon.NameEntity
 import org.jetbrains.dukat.astCommon.QualifierEntity
 import org.jetbrains.dukat.ownerContext.NodeOwner
 import org.jetbrains.dukat.tsmodel.InterfaceDeclaration
 import org.jetbrains.dukat.tsmodel.ModuleDeclaration
-import org.jetbrains.dukat.tsmodel.ParameterOwnerDeclaration
 import org.jetbrains.dukat.tsmodel.SourceFileDeclaration
 import org.jetbrains.dukat.tsmodel.SourceSetDeclaration
 import org.jetbrains.dukat.tsmodel.importClause.NamedImportsDeclaration
-import org.jetbrains.dukat.tsmodel.types.ParameterValueDeclaration
 
 private fun NameEntity.addPrefix(prefix: String): NameEntity {
     return when(this) {
@@ -19,11 +18,10 @@ private fun NameEntity.addPrefix(prefix: String): NameEntity {
     }
 }
 
-private class RenameImpossibleDeclarationsTypeLowering(private val namedImports: Set<IdentifierEntity>) : DeclarationTypeLowering {
+private class RenameImpossibleDeclarationsTypeLowering(private val namedImports: Set<IdentifierEntity>) : TopLevelDeclarationLowering {
 
     override fun lowerInterfaceDeclaration(declaration: InterfaceDeclaration, owner: NodeOwner<ModuleDeclaration>?): InterfaceDeclaration {
-        val declarationResolved = if (namedImports.contains(declaration.name)) { declaration.copy(name = declaration.name.addPrefix("Local")) } else { declaration }
-        return super.lowerInterfaceDeclaration(declarationResolved, owner)
+        return if (namedImports.contains(declaration.name)) { declaration.copy(name = declaration.name.addPrefix("Local")) } else { declaration }
     }
 }
 
