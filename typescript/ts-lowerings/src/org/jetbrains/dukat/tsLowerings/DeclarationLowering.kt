697063a11fabf3d95d2ef3e83dab4e1d27ab715b
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-04-08 15:25:31 +0200

    Revert "DeclarationLowering::lowerClassDeclaration, DeclarationLowering::lowerInterfaceDeclaration can actually return null"
    After a bit of thought I've realized that this can be approached slightly differently
    This reverts commit ebd4c7c5cce22c7ec6f1d110f4f6ce4b3289f899.

--- typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/DeclarationLowering.kt
+++ typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/DeclarationLowering.kt
@@ -30,8 +30,8 @@ import org.jetbrains.dukat.tsmodel.types.UnionTypeDeclaration
 interface DeclarationLowering {
     fun lowerVariableDeclaration(declaration: VariableDeclaration, owner: NodeOwner<ModuleDeclaration>?): VariableDeclaration
     fun lowerFunctionDeclaration(declaration: FunctionDeclaration, owner: NodeOwner<FunctionOwnerDeclaration>?): FunctionDeclaration
-    fun lowerClassDeclaration(declaration: ClassDeclaration, owner: NodeOwner<ModuleDeclaration>?): ClassDeclaration?
-    fun lowerInterfaceDeclaration(declaration: InterfaceDeclaration, owner: NodeOwner<ModuleDeclaration>?): InterfaceDeclaration?
+    fun lowerClassDeclaration(declaration: ClassDeclaration, owner: NodeOwner<ModuleDeclaration>?): ClassDeclaration
+    fun lowerInterfaceDeclaration(declaration: InterfaceDeclaration, owner: NodeOwner<ModuleDeclaration>?): InterfaceDeclaration
     fun lowerGeneratedInterfaceDeclaration(declaration: GeneratedInterfaceDeclaration, owner: NodeOwner<ModuleDeclaration>?): GeneratedInterfaceDeclaration
     fun lowerTypeDeclaration(declaration: TypeDeclaration, owner: NodeOwner<ParameterOwnerDeclaration>?): TypeDeclaration
     fun lowerFunctionTypeDeclaration(declaration: FunctionTypeDeclaration, owner: NodeOwner<ParameterOwnerDeclaration>?): FunctionTypeDeclaration
@@ -58,7 +58,7 @@ interface DeclarationLowering {
         }
     }
 
-    fun lowerClassLikeDeclaration(declaration: ClassLikeDeclaration, owner: NodeOwner<ModuleDeclaration>?): ClassLikeDeclaration? {
+    fun lowerClassLikeDeclaration(declaration: ClassLikeDeclaration, owner: NodeOwner<ModuleDeclaration>?): ClassLikeDeclaration {
         return when (declaration) {
             is InterfaceDeclaration -> lowerInterfaceDeclaration(declaration, owner)
             is ClassDeclaration -> lowerClassDeclaration(declaration, owner)
@@ -68,7 +68,7 @@ interface DeclarationLowering {
     }
 
     @Suppress("UNCHECKED_CAST")
-    fun lowerTopLevelDeclaration(declaration: TopLevelDeclaration, owner: NodeOwner<ModuleDeclaration>?): TopLevelDeclaration? {
+    fun lowerTopLevelDeclaration(declaration: TopLevelDeclaration, owner: NodeOwner<ModuleDeclaration>?): TopLevelDeclaration {
         return when (declaration) {
             is VariableDeclaration -> lowerVariableDeclaration(declaration, owner)
             is FunctionDeclaration -> lowerFunctionDeclaration(declaration, owner as NodeOwner<FunctionOwnerDeclaration>)
@@ -80,7 +80,7 @@ interface DeclarationLowering {
     }
 
     fun lowerTopLevelDeclarations(declarations: List<TopLevelDeclaration>, owner: NodeOwner<ModuleDeclaration>?): List<TopLevelDeclaration> {
-        return declarations.mapNotNull { declaration ->
+        return declarations.map { declaration ->
             lowerTopLevelDeclaration(declaration, owner)
         }
     }
