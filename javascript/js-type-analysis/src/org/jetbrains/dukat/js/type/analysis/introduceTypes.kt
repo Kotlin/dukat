dafb53e623f0eb7b548ff72bf84eed749e08ea45
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-04-13 14:26:28 +0200

    Forcibly turn top level declarations to external ones while deriving types from js

--- javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/analysis/introduceTypes.kt
+++ javascript/js-type-analysis/src/org/jetbrains/dukat/js/type/analysis/introduceTypes.kt
@@ -4,9 +4,14 @@ import org.jetbrains.dukat.js.type.exportResolution.CommonJSContext
 import org.jetbrains.dukat.js.type.exportResolution.TypeAnalysisContext
 import org.jetbrains.dukat.js.type.propertyOwner.Scope
 import org.jetbrains.dukat.panic.raiseConcern
+import org.jetbrains.dukat.tsmodel.ClassDeclaration
+import org.jetbrains.dukat.tsmodel.FunctionDeclaration
+import org.jetbrains.dukat.tsmodel.ModifierDeclaration
 import org.jetbrains.dukat.tsmodel.ModuleDeclaration
 import org.jetbrains.dukat.tsmodel.SourceFileDeclaration
 import org.jetbrains.dukat.tsmodel.SourceSetDeclaration
+import org.jetbrains.dukat.tsmodel.VariableDeclaration
+import org.jetbrains.dukat.tsmodel.WithModifiersDeclaration
 
 private fun ModuleDeclaration.collectConstraints(context: TypeAnalysisContext): Scope {
     val environment = context.getEnvironment()
@@ -16,18 +21,35 @@ private fun ModuleDeclaration.collectConstraints(context: TypeAnalysisContext): 
     calculateConstraints(environment, pathWalker)
 
     if (pathWalker.startNextPath()) {
-        raiseConcern("Conditional at top level not allowed!") {  }
+        raiseConcern("Conditional at top level not allowed!") { }
     }
 
     return environment
 }
 
-fun ModuleDeclaration.introduceTypes(context: TypeAnalysisContext) : ModuleDeclaration {
+private fun WithModifiersDeclaration.makeExternal(): List<ModifierDeclaration> {
+    return if (!hasDeclareModifier()) {
+        modifiers + ModifierDeclaration.DECLARE_KEYWORD
+    } else {
+        modifiers
+    }
+}
+
+fun ModuleDeclaration.introduceTypes(context: TypeAnalysisContext): ModuleDeclaration {
     val environment = collectConstraints(context)
 
     environment.resolveConstraints()
 
-    val declarations = context.getExportsFrom(environment, resourceName)
+    val declarations =
+            context.getExportsFrom(environment, resourceName)
+                    .map {
+                        when (it) {
+                            is FunctionDeclaration -> it.copy(modifiers = makeExternal())
+                            is VariableDeclaration -> it.copy(modifiers = makeExternal())
+                            is ClassDeclaration -> it.copy(modifiers = makeExternal())
+                            else -> it
+                        }
+                    }
 
     return copy(declarations = declarations)
 }
