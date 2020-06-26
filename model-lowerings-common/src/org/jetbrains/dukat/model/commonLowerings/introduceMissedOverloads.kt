eae5ca571137a72b2f9554fa77222eef350d454e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-06-26 16:15:49 +0200

    Use NamedCallableModel to reduce code duplication in introduceMissedOverloads

--- model-lowerings-common/src/org/jetbrains/dukat/model/commonLowerings/introduceMissedOverloads.kt
+++ model-lowerings-common/src/org/jetbrains/dukat/model/commonLowerings/introduceMissedOverloads.kt
@@ -7,6 +7,7 @@ import org.jetbrains.dukat.astModel.FunctionModel
 import org.jetbrains.dukat.astModel.InterfaceModel
 import org.jetbrains.dukat.astModel.MethodModel
 import org.jetbrains.dukat.astModel.ModuleModel
+import org.jetbrains.dukat.astModel.NamedCallableModel
 import org.jetbrains.dukat.astModel.ParameterModel
 import org.jetbrains.dukat.astModel.TypeModel
 import org.jetbrains.dukat.ownerContext.NodeOwner
@@ -18,9 +19,9 @@ private data class ModelData<T>(
 )
 
 private typealias ModelDataRecord<T> = MutableMap<List<TypeModel>, ModelData<T>>
-private typealias ModelsDataMap<T> = MutableMap<NameEntity, ModelDataRecord<T>>
+private typealias ModelsDataMap<T> = Map<NameEntity, ModelDataRecord<T>>
 
-private fun MethodModel.process(modelsDataMap: ModelsDataMap<MethodModel>) {
+private fun <CALLABLE_MODEL : NamedCallableModel> CALLABLE_MODEL.process(modelsDataMap: MutableMap<NameEntity, ModelDataRecord<CALLABLE_MODEL>>) {
     val nonOptionalHead = parameters.takeWhile { paramDeclaration ->
         paramDeclaration.initializer == null
     }
@@ -34,43 +35,27 @@ private fun MethodModel.process(modelsDataMap: ModelsDataMap<MethodModel>) {
 }
 
 
-private fun FunctionModel.process(modelsDataMap: ModelsDataMap<FunctionModel>) {
-    val nonOptionalHead = parameters.takeWhile { paramDeclaration ->
-        paramDeclaration.initializer == null
-    }
-
-    val headTypes = nonOptionalHead.map { parameterDeclaration -> parameterDeclaration.type }
-    val headNames = nonOptionalHead.map { parameterDeclaration -> parameterDeclaration.name }
-
-    val methodNodeRecord = modelsDataMap.getOrPut(name) { mutableMapOf() }
-    val methodData = methodNodeRecord.getOrPut(headTypes) { ModelData(mutableListOf(), headNames, this) }
-    methodData.optionalArgs.add(parameters.size - nonOptionalHead.size)
-}
-
+@OptIn(kotlin.ExperimentalStdlibApi::class)
 private fun ClassLikeModel.createDataMap(): ModelsDataMap<MethodModel> {
-    val methodsData: ModelsDataMap<MethodModel> = mutableMapOf()
-
-    members.forEach { member ->
-        if (member is MethodModel) {
-            member.process(methodsData)
+    return buildMap {
+        members.forEach { member ->
+            if (member is MethodModel) {
+                member.process(this)
+            }
         }
     }
-
-    return methodsData
 }
 
+@OptIn(kotlin.ExperimentalStdlibApi::class)
 private fun ModuleModel.createDataMap(): ModelsDataMap<FunctionModel> {
-    val nodeDataMap: ModelsDataMap<FunctionModel> = mutableMapOf()
-
-    declarations.forEach { member ->
-        if (member is FunctionModel) {
-            member.process(nodeDataMap)
+    return buildMap {
+        declarations.forEach { member ->
+            if (member is FunctionModel) {
+                member.process(this)
+            }
         }
     }
-
-    return nodeDataMap
 }
-
 
 private fun <T> Map.Entry<NameEntity, ModelDataRecord<T>>.process(
         generatedMethods: MutableList<T>,
@@ -95,29 +80,29 @@ private fun <T> Map.Entry<NameEntity, ModelDataRecord<T>>.process(
     }
 }
 
+@OptIn(kotlin.ExperimentalStdlibApi::class)
 private fun ModelsDataMap<MethodModel>.generateMethods(): List<MethodModel> {
-    val generatedMethods = mutableListOf<MethodModel>()
-    forEach { methodDataRecord ->
-        methodDataRecord.process(generatedMethods) { node, params -> node.copy(parameters = params) }
+    return buildList {
+        this@generateMethods.forEach { methodDataRecord ->
+            methodDataRecord.process(this) { node, params -> node.copy(parameters = params) }
+        }
     }
-
-    return generatedMethods
 }
 
+@OptIn(kotlin.ExperimentalStdlibApi::class)
 private fun ModelsDataMap<FunctionModel>.generateFunctions(): List<FunctionModel> {
-    val generatedFunctions = mutableListOf<FunctionModel>()
-    forEach { functionDataRecord ->
-        functionDataRecord.process(generatedFunctions) { node, params -> node.copy(parameters = params) }
+    return buildList {
+        this@generateFunctions.forEach { functionDataRecord ->
+            functionDataRecord.process(this) { node, params -> node.copy(parameters = params) }
+        }
     }
-
-    return generatedFunctions
 }
 
 private class IntroduceMissedOverloadsTypeLowering : ModelWithOwnerTypeLowering {
     override fun lowerInterfaceModel(ownerContext: NodeOwner<InterfaceModel>, parentModule: ModuleModel): InterfaceModel {
         val model = ownerContext.node
         return super.lowerInterfaceModel(ownerContext.copy(node = ownerContext.node.copy(
-            members = model.members + model.createDataMap().generateMethods()
+                members = model.members + model.createDataMap().generateMethods()
         )), parentModule)
     }
 
@@ -131,11 +116,10 @@ private class IntroduceMissedOverloadsTypeLowering : ModelWithOwnerTypeLowering 
     override fun lowerRoot(moduleModel: ModuleModel, ownerContext: NodeOwner<ModuleModel>): ModuleModel {
         val nodesDataMap = moduleModel.createDataMap()
         return super.lowerRoot(moduleModel.copy(
-            declarations = lowerTopLevelDeclarations(moduleModel.declarations, ownerContext, moduleModel) + nodesDataMap.generateFunctions()
+                declarations = lowerTopLevelDeclarations(moduleModel.declarations, ownerContext, moduleModel) + nodesDataMap.generateFunctions()
         ), ownerContext)
     }
 }
-
 
 class IntroduceMissedOverloads : ModelLowering {
     override fun lower(module: ModuleModel): ModuleModel {
