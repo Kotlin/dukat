b4a8d299241794f3d09047bfadcda76d5b3e4abc
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-06-29 15:35:06 +0200

    Fix unionType/aliases.d.kt

--- compiler/test/data/typescript/unionType/aliases.d.kt
+++ compiler/test/data/typescript/unionType/aliases.d.kt
@@ -39,6 +39,10 @@ external interface Parent {
     operator fun invoke(vararg children: Number): Foo
 }
 
+external interface `T$0` {
+    fun ping(): Boolean
+}
+
 external interface Argv<T> {
     fun <O> command(handler: (args: Array<O> /* Array<O> & `T$0` */) -> Unit = definedExternally): Argv<T>
 }
