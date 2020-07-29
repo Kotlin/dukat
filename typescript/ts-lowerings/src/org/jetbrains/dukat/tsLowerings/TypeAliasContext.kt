f15aed6fb9c42d9c1bbcfb16e3710a526dffb977
Alexey Trilis <alexey.trilis@jetbrains.com>
2020-07-29 23:36:03 +0300

    Process complex types in dealiasing of type parameters

--- typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/TypeAliasContext.kt
+++ typescript/ts-lowerings/src/org/jetbrains/dukat/tsLowerings/TypeAliasContext.kt
@@ -2,11 +2,15 @@ package org.jetbrains.dukat.tsLowerings
 
 import org.jetbrains.dukat.astCommon.NameEntity
 import org.jetbrains.dukat.ownerContext.NodeOwner
+import org.jetbrains.dukat.tsmodel.GeneratedInterfaceReferenceDeclaration
 import org.jetbrains.dukat.tsmodel.ParameterOwnerDeclaration
 import org.jetbrains.dukat.tsmodel.TypeAliasDeclaration
+import org.jetbrains.dukat.tsmodel.types.FunctionTypeDeclaration
+import org.jetbrains.dukat.tsmodel.types.IntersectionTypeDeclaration
 import org.jetbrains.dukat.tsmodel.types.ParameterValueDeclaration
 import org.jetbrains.dukat.tsmodel.types.TypeDeclaration
 import org.jetbrains.dukat.tsmodel.types.TypeParamReferenceDeclaration
+import org.jetbrains.dukat.tsmodel.types.UnionTypeDeclaration
 
 private class TypeSpecifierLowering(private val aliasParamMap: Map<NameEntity, ParameterValueDeclaration>) : DeclarationLowering {
 
@@ -14,6 +18,13 @@ private class TypeSpecifierLowering(private val aliasParamMap: Map<NameEntity, P
         val declarationResolved = when (declaration) {
             is TypeParamReferenceDeclaration -> aliasParamMap.getOrDefault(declaration.value, declaration)
             is TypeDeclaration -> aliasParamMap.getOrDefault(declaration.value, declaration)
+            is UnionTypeDeclaration -> declaration.copy(params = declaration.params.map { spec(it) })
+            is IntersectionTypeDeclaration -> declaration.copy(params = declaration.params.map { spec(it) })
+            is FunctionTypeDeclaration -> declaration.copy(
+                parameters = declaration.parameters.map { it.copy(type = spec(it.type)) },
+                type = spec(declaration.type)
+            )
+            is GeneratedInterfaceReferenceDeclaration -> declaration.copy(typeParameters = declaration.typeParameters.map { spec(it) })
             else -> declaration
         }
 
