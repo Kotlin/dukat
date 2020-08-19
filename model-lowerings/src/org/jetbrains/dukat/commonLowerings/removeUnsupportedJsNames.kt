b6c13b81f2d4084cf3adadd4ba1c58e9b7d00d67
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-08-19 12:48:30 +0200

    Always convert type to nullable while creating nativeGetters from literal properties

--- model-lowerings/src/org/jetbrains/dukat/commonLowerings/removeUnsupportedJsNames.kt
+++ model-lowerings/src/org/jetbrains/dukat/commonLowerings/removeUnsupportedJsNames.kt
@@ -4,6 +4,7 @@ import org.jetbrains.dukat.astCommon.IdentifierEntity
 import org.jetbrains.dukat.astCommon.NameEntity
 import org.jetbrains.dukat.astCommon.QualifierEntity
 import org.jetbrains.dukat.astModel.AnnotationModel
+import org.jetbrains.dukat.astModel.FunctionTypeModel
 import org.jetbrains.dukat.astModel.InterfaceModel
 import org.jetbrains.dukat.astModel.MemberModel
 import org.jetbrains.dukat.astModel.MethodModel
@@ -11,6 +12,8 @@ import org.jetbrains.dukat.astModel.ModuleModel
 import org.jetbrains.dukat.astModel.ParameterModel
 import org.jetbrains.dukat.astModel.PropertyModel
 import org.jetbrains.dukat.astModel.TypeModel
+import org.jetbrains.dukat.astModel.TypeParameterModel
+import org.jetbrains.dukat.astModel.TypeParameterReferenceModel
 import org.jetbrains.dukat.astModel.TypeValueModel
 import org.jetbrains.dukat.model.commonLowerings.ModelLowering
 import org.jetbrains.dukat.model.commonLowerings.ModelWithOwnerTypeLowering
@@ -63,6 +66,16 @@ private fun resolveCommonType(members: List<MemberModel>): TypeModel {
     } ?: TypeValueModel(IdentifierEntity("Any"), emptyList(), null, null, true)
 }
 
+private fun TypeModel.makeNullable(): TypeModel {
+    return when(this) {
+        is TypeValueModel -> copy(nullable = true)
+        is FunctionTypeModel -> copy(nullable = true)
+        is TypeParameterReferenceModel -> copy(nullable = true)
+        is TypeParameterModel -> copy(nullable = true)
+        else -> this
+    }
+}
+
 private class UnsupportedJsNamesLowering : ModelWithOwnerTypeLowering {
 
     override fun lowerInterfaceModel(ownerContext: NodeOwner<InterfaceModel>, parentModule: ModuleModel): InterfaceModel {
@@ -90,7 +103,7 @@ private class UnsupportedJsNamesLowering : ModelWithOwnerTypeLowering {
                             vararg = false,
                             modifier = null
                     )),
-                    type = commonType,
+                    type = commonType.makeNullable(),
                     typeParameters = emptyList(),
                     static = false,
                     override = null,
