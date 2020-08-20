9786600be510f13b4905e587aaf434836883e637
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-08-20 16:18:50 +0200

    fix nestedInheritedInterface

--- compiler/test/data/typescript/class/inheritance/nestedInheritedInterface.d.kt
+++ compiler/test/data/typescript/class/inheritance/nestedInheritedInterface.d.kt
@@ -1,4 +1,4 @@
-// [test] aaa.kt
+// [test] nestedInheritedInterface.kt
 @file:Suppress("INTERFACE_WITH_SUPERCLASS", "OVERRIDING_FINAL_MEMBER", "RETURN_TYPE_MISMATCH_ON_OVERRIDE", "CONFLICTING_OVERLOADS")
 
 import kotlin.js.*
