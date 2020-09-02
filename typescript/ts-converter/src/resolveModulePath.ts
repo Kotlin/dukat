e5a4c5380766743ce994a1e35b6d85730f1fedd4
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-09-03 00:41:04 +0200

    Don't normalize paths in resolveModulePath

--- typescript/ts-converter/src/resolveModulePath.ts
+++ typescript/ts-converter/src/resolveModulePath.ts
@@ -4,7 +4,7 @@ import * as ts from "typescript";
 export function resolveModulePath(node: ts.Expression): string | null {
   const module = ts.getResolvedModule(node.getSourceFile(), node.text);
   if (module && (typeof module.resolvedFileName == "string")) {
-    return tsInternals.normalizePath(module.resolvedFileName);
+    return module.resolvedFileName;
   }
   return null;
 }
