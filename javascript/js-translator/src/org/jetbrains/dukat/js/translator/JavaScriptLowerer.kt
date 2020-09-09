27e30ec5d23112325f8510faa70c3f25cf178165
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-09-09 22:17:51 +0200

    JavaScriptLowerer is a ECMAScriptLowerer

--- javascript/js-translator/src/org/jetbrains/dukat/js/translator/JavaScriptLowerer.kt
+++ javascript/js-translator/src/org/jetbrains/dukat/js/translator/JavaScriptLowerer.kt
@@ -3,13 +3,16 @@ package org.jetbrains.dukat.js.translator
 import org.jetbrains.dukat.astModel.SourceSetModel
 import org.jetbrains.dukat.js.type.analysis.introduceTypes
 import org.jetbrains.dukat.moduleNameResolver.ModuleNameResolver
+import org.jetbrains.dukat.ts.translator.ECMAScriptLowerer
 import org.jetbrains.dukat.ts.translator.TypescriptLowerer
 import org.jetbrains.dukat.tsLowerings.mergeDuplicates.mergeDuplicates
 import org.jetbrains.dukat.tsmodel.SourceSetDeclaration
 
-class JavaScriptLowerer(nameResolver: ModuleNameResolver) : TypescriptLowerer(nameResolver, null, true) {
+class JavaScriptLowerer(nameResolver: ModuleNameResolver) : ECMAScriptLowerer {
+    private val typescriptLowerer = TypescriptLowerer(nameResolver, null, true)
+
     override fun lower(sourceSet: SourceSetDeclaration): SourceSetModel {
-        return super.lower(
+        return typescriptLowerer.lower(
                 sourceSet
                         .introduceTypes()
                         .mergeDuplicates()
