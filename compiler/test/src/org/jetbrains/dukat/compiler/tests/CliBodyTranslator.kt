78840589bdc4f26a537bba6f0f0559e14dea4419
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-09-10 22:56:24 +0200

    CliBodyTranslator differs from CliTranslator only by translator

--- compiler/test/src/org/jetbrains/dukat/compiler/tests/CliBodyTranslator.kt
+++ compiler/test/src/org/jetbrains/dukat/compiler/tests/CliBodyTranslator.kt
@@ -1,21 +1,8 @@
 package org.jetbrains.dukat.compiler.tests
 
-import org.jetbrains.dukat.cli.compileUnits
 import org.jetbrains.dukat.moduleNameResolver.CommonJsNameResolver
-import org.jetbrains.dukat.translatorString.translateByteArray
 import org.jetbrains.dukat.ts.translator.createJsByteArrayWithBodyTranslator
 
 class CliBodyTranslator : CliTranslator() {
     override val translator = createJsByteArrayWithBodyTranslator(CommonJsNameResolver(), null)
-
-    override fun convert(
-            input: String,
-            tsConfig: String?,
-            dirName: String,
-            reportPath: String?
-    ) {
-        val binData = translateBinary(input, tsConfig)
-        val translatedUnits = translateByteArray(binData, translator)
-        compileUnits(translatedUnits, dirName, reportPath)
-    }
 }
