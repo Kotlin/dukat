1a6987a0b4fe6c025ad304510d78a7825475483a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-10-15 19:23:20 +0200

    AnnotationModel with optional Target

--- ast-model/src/org/jetbrains/dukat/astModel/AnnotationModel.kt
+++ ast-model/src/org/jetbrains/dukat/astModel/AnnotationModel.kt
@@ -2,11 +2,13 @@ package org.jetbrains.dukat.astModel
 
 import org.jetbrains.dukat.astCommon.Entity
 import org.jetbrains.dukat.astCommon.NameEntity
+import org.jetbrains.dukat.astCommon.toNameEntity
 
 data class AnnotationModel(
         val name: String,
-        val params: List<NameEntity>
-) : Entity {
+        val params: List<NameEntity>,
+        val target: AnnotationTarget? = null
+        ) : Entity {
     companion object {
         val NATIVE_GETTER  = AnnotationModel("nativeGetter", emptyList())
         val NATIVE_SETTER  = AnnotationModel("nativeSetter", emptyList())
