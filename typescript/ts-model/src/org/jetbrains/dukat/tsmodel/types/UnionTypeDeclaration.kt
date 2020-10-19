4f31485448a035caaa47ef89cef213122ab85549
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2020-10-19 22:50:04 +0200

    Treat StringLiteralUnions | string as StringLiteralUnion (the same with numeric literals and numbers)

--- typescript/ts-model/src/org/jetbrains/dukat/tsmodel/types/UnionTypeDeclaration.kt
+++ typescript/ts-model/src/org/jetbrains/dukat/tsmodel/types/UnionTypeDeclaration.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.dukat.tsmodel.types
 
+import org.jetbrains.dukat.astCommon.IdentifierEntity
 import org.jetbrains.dukat.astCommon.MetaData
 import org.jetbrains.dukat.tsmodel.ParameterOwnerDeclaration
 
@@ -10,10 +11,32 @@ data class UnionTypeDeclaration(
         override var meta: MetaData? = null
 ) : ParameterValueDeclaration, ParameterOwnerDeclaration
 
+private fun ParameterValueDeclaration.isString(): Boolean {
+    return ((this as? TypeDeclaration)?.value as? IdentifierEntity)?.value == "String"
+}
+
+private fun ParameterValueDeclaration.isNumber(): Boolean {
+    return ((this as? TypeDeclaration)?.value as? IdentifierEntity)?.value == "Number"
+}
+
 fun UnionTypeDeclaration.canBeTranslatedAsStringLiteral(): Boolean {
-    return params.all { it is StringLiteralDeclaration }
+    val paramsList = params.toMutableList()
+    val isStringLiteralUnion =  paramsList.all { it is StringLiteralDeclaration }
+    if (isStringLiteralUnion) {
+        return true
+    }
+
+    paramsList.removeAll { it.isString() }
+    return paramsList.all { it is StringLiteralDeclaration }
 }
 
 fun UnionTypeDeclaration.canBeTranslatedAsNumericLiteral(): Boolean {
+    val paramsList = params.toMutableList()
+    val isNumericLiteralUnion =  paramsList.all { it is NumericLiteralDeclaration }
+    if (isNumericLiteralUnion) {
+        return true
+    }
+
+    paramsList.removeAll { it.isNumber() }
     return params.all { it is NumericLiteralDeclaration }
 }
